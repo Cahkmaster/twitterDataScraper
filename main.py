f3b5dd8be61d035b518ef7a38781e35581f9fa99
Ish <93486637+IsmaeelHashimi@users.noreply.github.com>
2023-01-16 00:08:11 -0500

    Update main.py
    Added an option to let the user filter retweets.

--- main.py
+++ main.py
@@ -20,13 +20,21 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-# change keywords to whatever you want to search twitter for
-keywords = input('What keywords do you want to include in this search?: ')
+# variables to store user input
+
+keywords = input('What keyword(s) do you want to include in this search?: ')
+rtOrNo = input('Do you want retweets to be included in the search? y/n:  ')
+
+# if statement to decide if query will include retweets
+
+if rtOrNo == 'n':
+    keywords = keywords  + ' -filter:retweets'
+
 limit = int(input('How many tweets do you want to pull?: '))
 language = str(input('In what language do you want the tweets to be in? (use two letter abbreviations only): '))
 
 # used limit to bypass tweet per command limit
-tweets = tweepy.Cursor(api.search_tweets, q=keywords, count = 100, tweet_mode = 'extended', lang = language).items(limit)
+tweets = tweepy.Cursor(api.search_tweets, q=keywords, count = 100, tweet_mode = 'extended', lang=language).items(limit)
 
 # setting up dataframe
 columns = ['Time', 'User', 'Tweet']
@@ -41,7 +49,6 @@ df = pd.DataFrame(data, columns = columns)
 
 # creates a csv file with dataframe info
 df.to_csv('tweets.csv')
-
 
 
 
